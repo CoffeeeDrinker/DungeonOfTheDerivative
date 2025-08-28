6ddb791071df225399f4812573706a1b13d5aedf
Kasey Swenson <kasey.swenson@icloud.com>
2025-08-28 15:59:18 -0500

    Added Player Movement Script with parameters for walk and idle animations

---
+++ Assets/Kasey's Stuff (DO NOT OPEN)/Kasey Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Player Movement Stuff
+    public Rigidbody2D PlayerRB;
+    public float speed;
+    private Vector2 move = new Vector2();
+
+    //Player sprites/animation
+    public Animator PlayerAnim;
+    private string currentAnim = "idleDown";
+
+    void Update()
+    {
+        //Move Player
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+        PlayerRB.velocity = move * speed * Time.deltaTime;
+
+        //Walk Animation
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) { //Choose walk direction if moving
+            if(Input.GetAxisRaw("Horizontal") < 0){
+                PlayAnimation("walkLeft");
+            } else if(Input.GetAxisRaw("Horizontal") > 0){
+                PlayAnimation("walkRight");
+            } else if(Input.GetAxisRaw("Vertical") > 0){
+                PlayAnimation("walkUp");
+            } else if(Input.GetAxisRaw("Vertical") < 0){
+                PlayAnimation("walkDown");
+            } 
+        } else{ //Find direction to play idle animation in
+            Debug.Log("idle"+currentAnim.Substring(4));
+            PlayAnimation("idle"+currentAnim.Substring(4));
+        }
+    }
+
+    public void PlayAnimation(string anim){
+        PlayerAnim.ResetTrigger(currentAnim);
+        PlayerAnim.SetTrigger(anim);
+        currentAnim = anim;
+    }
+}
