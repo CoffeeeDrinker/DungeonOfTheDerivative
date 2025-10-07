0a191a6dc99579679c70525872cfe8ccdc473b41
Kasey Swenson <kasey.swenson@icloud.com>
2025-10-07 16:08:36 -0500

    Added the intro story + holy grail scripts, prefabs, etc

--- Assets/Kasey's Stuff (DO NOT OPEN)/Kasey Scripts/StorybookScript.cs
+++ Assets/Kasey's Stuff (DO NOT OPEN)/Kasey Scripts/StorybookScript.cs
@@ -25,6 +25,19 @@ public class StorybookScript : MonoBehaviour
     public Effect_Color typewriterEffect;
     private bool typewriterEffectComplete = true;
 
+    //Holy Grail Stuff
+    public Canvas canvas;
+    public List<GameObject> holyGrailPrefabs;
+
+    public Transform holyGrailHolder;
+    public List<GameObject> holyGrails;
+
+    public Transform blackHoleCenter;
+
+    public float holyGrailSpeed;
+    public float holyGrailRotationSpeed;
+    Vector3 randLocation = new Vector3(0, 0, 0);
+
     //Player Stuff
     public PlayerMovement playerScript;
 
@@ -36,6 +49,30 @@ public class StorybookScript : MonoBehaviour
         {
             storyLines.Add(file.Substring(0, file.IndexOf("\n")));
             file = file.Substring(file.IndexOf("\n")+1);
+        }
+    }
+
+    void Update()
+    {
+        //Check if we should summon the Holy Grails (line is 13)
+        if (storyLineI == 2)
+        {
+            //SUMMON THE HOLY GRAILS
+            SummonHolyGrail();
+        }
+        
+        for(int i = 0; i < holyGrails.Count; i++)
+        {
+            //Move the holy grail
+            holyGrails[i].transform.position = Vector3.MoveTowards(holyGrails[i].transform.position, blackHoleCenter.position, holyGrailSpeed * Time.deltaTime);
+            holyGrails[i].transform.Rotate(new Vector3(0, 0, holyGrailRotationSpeed * Time.deltaTime));
+
+            //Check if we should delete the holy grail
+            if (holyGrails[i].transform.position == blackHoleCenter.position)
+            {
+                Destroy(holyGrails[i]);
+                holyGrails.RemoveAt(i);
+            }
         }
     }
 
@@ -84,5 +121,29 @@ public class StorybookScript : MonoBehaviour
     public void SetTypewriterComplete()
     {
         typewriterEffectComplete = true;
+    }
+
+    public void SummonHolyGrail()
+    {
+        int randNum = Random.Range(0, 6);
+        switch (randNum){
+            case 0: //Spawn above screen
+                randLocation.x = Random.Range(-200, canvas.pixelRect.width+200);
+                randLocation.y = Random.Range(canvas.pixelRect.height, canvas.pixelRect.height+200);
+                break;
+            case 1: //Spawn below screen
+                randLocation.x = Random.Range(-200, canvas.pixelRect.width + 200);
+                randLocation.y = Random.Range(-200, 0);
+                break;
+            case 2: //Spawn left of screen
+                randLocation.x = Random.Range(-200, 0);
+                randLocation.y = Random.Range(-200, canvas.pixelRect.height + 200);
+                break;
+            case 3: //Spawn right of screen
+                randLocation.x = Random.Range(canvas.pixelRect.width, canvas.pixelRect.width + 200);
+                randLocation.y = Random.Range(-200, canvas.pixelRect.height + 200);
+                break;
+        }
+        holyGrails.Add(Instantiate(holyGrailPrefabs[Random.Range(0, holyGrailPrefabs.Count)], randLocation, Quaternion.identity, holyGrailHolder));
     }
 }
