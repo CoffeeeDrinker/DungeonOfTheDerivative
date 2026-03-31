19e5e81f4ce2a3315ac464a85f259d5220c5fad1
Kasey Swenson <kasey.swenson@icloud.com>
2026-03-31 16:12:29 -0500

    Added more colliders and stupid doors that dont work I hate them

--- Assets/Kasey's Stuff (DO NOT OPEN)/Kasey Scripts/ClockHand.cs
+++ Assets/Kasey's Stuff (DO NOT OPEN)/Kasey Scripts/ClockHand.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -8,10 +9,55 @@ public class ClockHand : MonoBehaviour
     public Vector3 pivotPoint;
     public float rotationSpeed;
 
-    public float rotateDirection; //1 for counterclockwise, -1 for clockwise, 0 for no rotation
+    private float rotateDirection; //1 for counterclockwise, -1 for clockwise, 0 for no rotation
+    private float rotationAngle = 180;
+
+    public float goToAngle;
 
     void Update()
     {
+        if ((int)rotationAngle == (int)goToAngle)
+        {
+            transform.RotateAround(pivotPoint, Vector3.forward, rotateDirection * Mathf.Abs(rotationAngle-goToAngle));
+            rotateDirection = 0;
+        }
+        else
+        {
+            if(rotateDirection == 0)
+            {
+                GetNewRotateDirection();
+            }
+        }
+
         transform.RotateAround(pivotPoint, Vector3.forward, rotateDirection * rotationSpeed * Time.deltaTime);
+
+        if(rotationAngle >= 360)
+        {
+            rotationAngle -= 360;
+        } else if (rotationAngle <= 0)
+        {
+            rotationAngle += 360;
+        }
+        rotationAngle -= rotateDirection * rotationSpeed * Time.deltaTime;
+    }
+
+    private void GetNewRotateDirection()
+    {
+        float angle = goToAngle - rotationAngle;
+        if(angle < 0)
+        {
+            angle += 360;
+        }
+
+        if(angle-rotationAngle < 0)
+        {
+            rotateDirection = -1;
+        }
+        else
+        {
+            rotateDirection = 1;
+        }
+
+        
     }
 }
